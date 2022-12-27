77fbbb7ed5e5de766b132db2098982868351482a
yvan <https://github.com/Pallies>
2022-12-27 19:02:22 +0100

    function anagram

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ### AlgorithmeJS
 description d'algorithme
 
+install
+npm i typescript
+
+# run js
+node file.js
+
+# compile ts to js
+tsc file.ts
+
+## OR
+
+
+
+
+
---
+++ src/functions/anagram.ts
@@ -0,0 +1,26 @@
+function anagram(...str: string[]): boolean {
+  let a: any = {};
+  let b: any = {};
+  if (str[0].length !== str[1].length) return false;
+  for (let val of str[0]) a[val] = ++a[val] || 1;
+  for (let val of str[1]) b[val] = ++b[val] || 1;
+  for (let key in a) {
+    if (!b.hasOwnProperty(key)) return false;
+    if (b[key] !== a[key]) return false;
+  }
+  return true;
+}
+var response = anagram("", "");
+console.log("" + response);
+response = anagram("aaz", "zza");
+console.log("aaz " + response);
+response = anagram("anagram", "nagaram");
+console.log("anagram " + response);
+response = anagram("rat", "car");
+console.log("rat " + response);
+response = anagram("awesome", "awesom");
+console.log("awesome " + response);
+response = anagram("qwerty", "qeywrt");
+console.log("qwerty " + response);
+response = anagram("texttwisttime", "timetwisttext");
+console.log("texttwistting " + response);
