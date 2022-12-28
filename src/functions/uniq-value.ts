949cdc0391adfd745b4456c9fa7dd54857620e03
yvan <https://github.com/Pallies>
2022-12-28 13:00:12 +0100

    algo

---
+++ src/functions/uniq-value.ts
@@ -0,0 +1,18 @@
+// compter le nombre d'occurence dans une chaine
+function uniqValue(arg: any): number {
+  if (arg.length == 0) return 0;
+  let result: {[key:number]:number} = {};
+  for (let v of arg) {
+    result[v] = ++result[v] || 0;
+  }
+  let count:number=0
+  for(let k in result){
+    count++;
+  }
+  return count;
+}
+
+console.log(uniqValue([1,1,1,1,2,2]));
+console.log(uniqValue([1,2,3,4,4,4,7,7,12,12,13]))
+console.log(uniqValue([]))
+console.log(uniqValue([-2,-1,-1,0,1]))
