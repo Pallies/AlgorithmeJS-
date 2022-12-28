78e67beeeb998cd6e82cd8df2edda4b458430bac
yvan <https://github.com/Pallies>
2022-12-28 13:20:40 +0100

    other method

--- src/functions/uniq-value.ts
+++ src/functions/uniq-value.ts
@@ -29,6 +29,17 @@ function uniqValuePointeur(arg: any) {
   }
   return left == 0 ? 0 : left + 1;
 }
+/** avec deux pointeur sur une liste triée */
+function uniqValuePointeurFor(arg: any) {
+  let left=0;
+    for (let rigth=1;rigth < arg.length;rigth++) {
+      if (arg[left] != arg[rigth]) {
+        left++;
+        arg[left] = arg[rigth];
+      }
+    }
+    return left == 0 ? 0 : left + 1;
+  }
 var time1 = performance.now();
 console.log(uniqValue([1, 1, 1, 1, 2, 2]));
 console.log(uniqValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
@@ -44,3 +55,11 @@ console.log(uniqValuePointeur([]));
 console.log(uniqValuePointeur([-2, -1, -1, 0, 1]));
 var time4 = performance.now();
 console.log(`Time 1: ${(time4 - time3) / 1000} seconds.`);
+
+var time5 = performance.now();
+console.log(uniqValuePointeurFor([1, 1, 1, 1, 2, 2]));
+console.log(uniqValuePointeurFor([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(uniqValuePointeurFor([]));
+console.log(uniqValuePointeurFor([-2, -1, -1, 0, 1]));
+var time6 = performance.now();
+console.log(`Time 1: ${(time6 - time5) / 1000} seconds.`);
