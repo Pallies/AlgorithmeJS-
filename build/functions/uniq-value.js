07f83330c6e2c44e59fd994d8c0d5fffff5415e4
yvan <https://github.com/Pallies>
2022-12-28 13:16:25 +0100

    best solution

--- build/functions/uniq-value.js
+++ build/functions/uniq-value.js
@@ -14,7 +14,34 @@ function uniqValue(arg) {
     }
     return count;
 }
+// O(log n)
+/** avec deux pointeur sur une liste triée */
+function uniqValuePointeur(arg) {
+    let left = 0;
+    let rigth = 1;
+    while (rigth < arg.length) {
+        if (arg[left] != arg[rigth]) {
+            left++;
+            arg[left] = arg[rigth];
+            rigth++;
+        }
+        else {
+            rigth++;
+        }
+    }
+    return left == 0 ? 0 : left + 1;
+}
+var time1 = performance.now();
 console.log(uniqValue([1, 1, 1, 1, 2, 2]));
 console.log(uniqValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(uniqValue([]));
 console.log(uniqValue([-2, -1, -1, 0, 1]));
+var time2 = performance.now();
+console.log(`Time 1: ${(time2 - time1) / 1000} seconds.`);
+var time3 = performance.now();
+console.log(uniqValuePointeur([1, 1, 1, 1, 2, 2]));
+console.log(uniqValuePointeur([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(uniqValuePointeur([]));
+console.log(uniqValuePointeur([-2, -1, -1, 0, 1]));
+var time4 = performance.now();
+console.log(`Time 1: ${(time4 - time3) / 1000} seconds.`);
--- src/functions/uniq-value.ts
+++ src/functions/uniq-value.ts
@@ -2,18 +2,45 @@
 /** compter le nombre d'occurence dans une chaine */
 function uniqValue(arg: any): number {
   if (arg.length == 0) return 0;
-  let result: {[key:number]:number} = {};
+  let result: { [key: number]: number } = {};
   for (let v of arg) {
     result[v] = ++result[v] || 0;
   }
-  let count:number=0
-  for(let k in result){
+  let count: number = 0;
+  for (let k in result) {
     count++;
   }
   return count;
 }
+// O(log n)
+/** avec deux pointeur sur une liste triée */
+function uniqValuePointeur(arg: any) {
+  let left: number = 0;
+  let rigth: number = 1;
 
-console.log(uniqValue([1,1,1,1,2,2]));
-console.log(uniqValue([1,2,3,4,4,4,7,7,12,12,13]))
-console.log(uniqValue([]))
-console.log(uniqValue([-2,-1,-1,0,1]))
+  while (rigth < arg.length) {
+    if (arg[left] != arg[rigth]) {
+      left++;
+      arg[left] = arg[rigth];
+      rigth++;
+    } else {
+      rigth++;
+    }
+  }
+  return left == 0 ? 0 : left + 1;
+}
+var time1 = performance.now();
+console.log(uniqValue([1, 1, 1, 1, 2, 2]));
+console.log(uniqValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(uniqValue([]));
+console.log(uniqValue([-2, -1, -1, 0, 1]));
+var time2 = performance.now();
+console.log(`Time 1: ${(time2 - time1) / 1000} seconds.`);
+
+var time3 = performance.now();
+console.log(uniqValuePointeur([1, 1, 1, 1, 2, 2]));
+console.log(uniqValuePointeur([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(uniqValuePointeur([]));
+console.log(uniqValuePointeur([-2, -1, -1, 0, 1]));
+var time4 = performance.now();
+console.log(`Time 1: ${(time4 - time3) / 1000} seconds.`);
